7621c5b7bdf02a7da65a7c4b779ef26ea2506b2e
Kaushik K <kaushikk@Kaushiks-MacBook-Air.local>
2024-01-08 08:03:15 +0530

    added final changes

--- 01/pizza-menu/src/index.js
+++ 01/pizza-menu/src/index.js
@@ -7,42 +7,42 @@ const pizzaData = [
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "pizzas/focaccia.jpg",
+    photoName: "img/focaccia.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "pizzas/margherita.jpg",
+    photoName: "img/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "pizzas/spinaci.jpg",
+    photoName: "img/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "pizzas/funghi.jpg",
+    photoName: "img/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "pizzas/salamino.jpg",
+    photoName: "img/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "pizzas/prosciutto.jpg",
+    photoName: "img/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -61,12 +61,11 @@ const Menu = (props) => {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
-        name="Pizza Spinachi"
-        ingredients="Tomato, mozarella, and pepperoni"
-        price="15"
-        photoName="img/salamino.jpg"
-      />
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (<Pizza pizzaObjeact={pizza} key={pizza.name}/>))}
+      </ul>
+
     </main>
   );
 };
@@ -74,13 +73,13 @@ const Menu = (props) => {
 
 function Pizza(props) {
     return (
-      <div className="pizza">
-        <img src={props.photoName} alt={props.name}></img>
+      <li className="pizza">
+        <img src={props.pizzaObjeact.photoName} alt={props.pizzaObjeact.name}></img>
         <div>
-            <h3>{props.name}</h3>
-            <p>{props.ingredients}</p>
+            <h3>{props.pizzaObjeact.name}</h3>
+            <p>{props.pizzaObjeact.ingredients}</p>
         </div>
-      </div>
+      </li>
     );
   }
 
